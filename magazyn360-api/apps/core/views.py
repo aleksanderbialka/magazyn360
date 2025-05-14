68ad2aa9a8fbd7b29384105e26902d55df9af474
aleksanderbialka <aleksander.bialka@icloud.com>
2025-05-14 22:50:40 +0200

    Added JWT authentication and user info endpoint (/me), refactor core URLs

--- magazyn360-api/apps/core/views.py
+++ magazyn360-api/apps/core/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from apps.core.serializers import UserSerializer
+
+
+class MeView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
