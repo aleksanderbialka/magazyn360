9eeabf61c7d637bf9412e8ec20d59f2c7867d403
aleksanderbialka <aleksander.bialka@icloud.com>
2025-05-24 16:43:01 +0200

    changed actions

--- magazyn360-api/bin/run_tests.sh
+++ magazyn360-api/bin/run_tests.sh
@@ -5,4 +5,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/.."
 
+source /var/www/magazyn360_api/venv/bin/activate
 pytest --cov=magazyn360_api --cov-report=html --cov-report=term-missing .
