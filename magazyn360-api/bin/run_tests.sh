95828f27bd8295e327c897e368851503dc928d08
aleksanderbialka <aleksander.bialka@icloud.com>
2025-05-28 22:00:14 +0200

    Fixed tests coverage to cicd

--- magazyn360-api/bin/run_tests.sh
+++ magazyn360-api/bin/run_tests.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/.."
 
 source /var/www/magazyn360/app/magazyn360_env/bin/activate
-pytest --cov=magazyn360_api --cov-report=term-missing --junitxml=pytest_results.xml
+pytest --cov=magazyn360_api --cov-report=term-missing --junitxml=pytest-results.xml
